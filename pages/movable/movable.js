85275dd2a228deb0ce985ce7f41b3d824ddb1df7
xueyan <348352046@qq.com>
2019-05-26 19:52:03 +0800

    update

---
+++ pages/movable/movable.js
@@ -0,0 +1,45 @@
+// pages/movable/movable.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    items: ['/image/img1.jpg', '/image/img2.jpg', '/image/img3.jpg', '/image/img4.jpg', '/image/img5.jpg', '/image/img6.jpg'],
+    tempItems: [],
+    index: 0
+  },
+  tap(e) {
+    let index = e.currentTarget.dataset.index
+    console.log(index)
+    let item = this.data.items.shift()
+    this.data.tempItems.unshift(item)
+    this.setData({
+      items: this.data.items,
+      tempItems: this.data.tempItems
+    })
+  },
+  // 上一张
+  last() {
+    let item = this.data.tempItems.shift()
+    if (item) {
+      this.data.items.unshift(item)
+      this.setData({
+        items: this.data.items,
+        tempItems: this.data.tempItems
+      })
+    }
+  },
+  // 下一张
+  next() {
+    if (this.data.items.length > 1) {
+      let item = this.data.items.shift()
+      this.data.tempItems.unshift(item)
+      this.setData({
+        items: this.data.items,
+        tempItems: this.data.tempItems
+      })
+    }
+  }
+
+})
